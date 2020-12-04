7629c7a179c18b949db619dc547b1632512ac3cd
bionascu <beatrice.nsc@gmail.com>
2020-12-04 22:56:33 +0100

    day 4 part 1

---
+++ day4/day4_part1.py
@@ -0,0 +1,27 @@
+# advent of code 
+# day 4
+
+filename = 'input_test.txt'
+
+expected_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+valid_entry_count = 0
+expected_field_count = 0
+
+with open(filename, 'r') as file:
+	lines = file.readlines()
+
+	for line in lines:
+
+		if line == '\n':
+			expected_field_count = 0
+
+		else:
+			parts = line.split(' ')
+			for part in parts:
+				field, value = part.split(':')
+				if field in expected_fields:
+					expected_field_count += 1
+			if expected_field_count == len(expected_fields):
+				valid_entry_count += 1 
+
+print(valid_entry_count)
