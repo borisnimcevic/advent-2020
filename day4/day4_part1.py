644b839bbcb3717b546f0818bf7413cf5743f8b1
Boris Nimcevic <borisnimcevic@gmail.com>
2020-12-04 23:34:38 +0100

    Correct answer for part 1.

--- day4/day4_part1.py
+++ day4/day4_part1.py
@@ -1,27 +1,25 @@
 # advent of code 
 # day 4
 
-filename = 'input_test.txt'
+filename = 'input.txt'
 
 expected_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 valid_entry_count = 0
 expected_field_count = 0
 
 with open(filename, 'r') as file:
-	lines = file.readlines()
+    lines = file.readlines()
+    for line in lines:
+        if line == '\n':
+            expected_field_count = 0
+        else:
+            parts = line.split(' ')
+            for part in parts:
+                field, value = part.split(':')
+                if field in expected_fields:
+                    expected_field_count += 1
+            if expected_field_count == len(expected_fields):
+                expected_field_count = 0
+                valid_entry_count += 1
 
-	for line in lines:
-
-		if line == '\n':
-			expected_field_count = 0
-
-		else:
-			parts = line.split(' ')
-			for part in parts:
-				field, value = part.split(':')
-				if field in expected_fields:
-					expected_field_count += 1
-			if expected_field_count == len(expected_fields):
-				valid_entry_count += 1 
-
-print(valid_entry_count)
+print("final " + str(valid_entry_count))
